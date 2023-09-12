b66936a566eee2a3d5fa10672b556aaa7489f232
Konstantyn <gagarin2789@gmail.com>
2023-09-12 20:00:42 +0200

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,5 +91,4 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.13.0")
     ksp("com.squareup.moshi:moshi-kotlin-codegen:1.13.0")
-
 }
