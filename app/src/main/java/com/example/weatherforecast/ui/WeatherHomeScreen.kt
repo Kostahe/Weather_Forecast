340e0f92805c9d39e5597863b9f104c88a80064c
Konstantyn <gagarin2789@gmail.com>
2023-09-16 00:20:43 +0200

    Fixed bug with wrong request

--- app/src/main/java/com/example/weatherforecast/ui/WeatherHomeScreen.kt
+++ app/src/main/java/com/example/weatherforecast/ui/WeatherHomeScreen.kt
@@ -5,14 +5,19 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.weatherforecast.R
@@ -38,7 +43,6 @@ fun WeatherHomeScreen() {
                     modifier = Modifier.align(Alignment.Center)
                 )
             }
-
         }
         is State.Success -> {
             Text(text = "Success")
@@ -52,11 +56,17 @@ fun WeatherHomeScreen() {
                 Image(
                     painter = painterResource(id = R.drawable.ic_location_off),
                     contentDescription = null,
-                    Modifier.size(25.dp)
-
+                    modifier = Modifier.size(25.dp)
                 )
+                Spacer(modifier = Modifier.height(10.dp))
+                Card {
+                    Text(
+                        text = weatherState.message.toString(),
+                        textAlign = TextAlign.Center
+                    )
+                }
+
             }
-            
             Log.e("MyError", "Error message: ${weatherState.message}")
         }
     }
