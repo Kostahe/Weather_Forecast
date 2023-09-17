f515ebb17543378a7569d15f4253a6c2e1e90ccc
Konstantyn <gagarin2789@gmail.com>
2023-09-17 14:28:53 +0200

    Changing Colors

--- app/src/main/java/com/example/weatherforecast/ui/theme/Theme.kt
+++ app/src/main/java/com/example/weatherforecast/ui/theme/Theme.kt
@@ -1,22 +1,12 @@
 package com.example.weatherforecast.ui.theme
 
-import android.app.Activity
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
+
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
-private val Colors = ColorScheme(
+
+private val colors = ColorScheme(
     primary = md_theme_primary,
     onPrimary = md_theme_onPrimary,
     primaryContainer = md_theme_primaryContainer,
@@ -50,9 +40,9 @@ private val Colors = ColorScheme(
 
 @Composable
 fun WeatherForecastTheme(
-    content: @Composable() () -> Unit
+    content: @Composable () -> Unit
 ) {
-    val colors = Colors
+    val colors = colors
 
     MaterialTheme(
         colorScheme = colors,
