0f80cb302e1a6b342a9cec6022390567794486f4
Konstantyn <gagarin2789@gmail.com>
2023-09-17 14:43:48 +0200

    WeatherDailyData finished

--- app/src/main/java/com/example/weatherforecast/ui/WeatherDailyData.kt
+++ app/src/main/java/com/example/weatherforecast/ui/WeatherDailyData.kt
@@ -1,12 +1,15 @@
 package com.example.weatherforecast.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -23,10 +26,8 @@ fun WeatherDailyData(
             items(data!!.weatherInfo) {
                 WeatherDailyDataItem(weatherInfo = it)
             }
-
         }
     }
-
 }
 
 @Composable
@@ -35,13 +36,16 @@ fun WeatherDailyDataItem(
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = modifier
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier.width(80.dp)
+            .height(100.dp)
     ) {
         Text(text = weatherInfo.time)
         Image(
             painter = painterResource(id = weatherInfo.weatherStatus.iconRes),
             contentDescription = stringResource(id = weatherInfo.weatherStatus.weatherDesc),
-            modifier = modifier.height(40.dp)
+            modifier = modifier.width(40.dp)
         )
         Text(text = "${weatherInfo.temperatureMax}°C")
     }
